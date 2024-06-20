9dd381d99693d8929f6a1c7957234a5240e9a4ea
AnujDeshwal <anujdeshwal95@gmail.com>
2024-06-20 19:41:33 +0530

    last

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ export const corsOptions = {
     origin: [
       "http://localhost:5174",
       "http://localhost:5173",
-      "https://namasate-talk-frontend.vercel.app/"
+      "https://namasate-talk-frontend.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
